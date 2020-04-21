8000e266930dc0d2318bc77b932492c59401a622
Mike Schwörer <pubgit@mikescher.com>
2020-04-21 11:16:01 +0200

    Initial (tray icon demo code)

---
+++ Source/ChocoTrayNotify/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows;
+
+namespace ChocoTrayNotify
+{
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
